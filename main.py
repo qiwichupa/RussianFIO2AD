8ca8cffbb504e17543146567ea4776be3d8da660
qiwichupa <qiwichupa@gmail.com>
2021-09-22 18:01:37 +0300

    v0.1.0

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from ui_files import main
 
 
 __appname__ = "RussianFIO2AD"
-__version__ = "0.0.10"
+__version__ = "0.1.0"
 
 # set working directory
 appdirs = AppDirs(__appname__, isportable=True, portabledatadirname='data')
